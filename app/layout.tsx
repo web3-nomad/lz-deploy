8df637fc48f6c6ea430290904ddbac2aafa58737
Web3 Nomad <102574104+web3-nomad@users.noreply.github.com>
2025-07-30 19:19:16 -0500

    fix metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Layer Zero Metadata',
+  description: 'Created with Web3NomadAI',
+  generator: 'Web3NomadAI',
 }
 
 export default function RootLayout({
